6a46a7195bcce3031700a2ee5d03c6a98d0fee98
Pedro Cardoso <036pedro.cardoso@gmail.com>
2023-04-08 18:58:12 -0300

    Sistema de Login simples

---
+++ Login-System/User.java
@@ -0,0 +1,12 @@
+public class User {
+
+    private String user = "User.01";
+    private String password = "9022003";
+    
+    public String getUser(){
+        return user;
+    }
+    public String getPass(){
+        return password;
+    }
+}
