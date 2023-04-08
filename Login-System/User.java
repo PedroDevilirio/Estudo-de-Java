320874d473ba207b659baa4f62160d0cb9d5301f
Pedro Cardoso <036pedro.cardoso@gmail.com>
2023-04-08 19:36:00 -0300

    Funções SET foram definidas no arquivo User.java

---
+++ Login-System/User.java
@@ -0,0 +1,24 @@
+public class User {
+
+    private String user = "User.01";
+    private String password = "9022003";
+    
+
+    //Métodos GET
+
+    public String getUser(){
+        return user;
+    }
+    public String getPass(){
+        return password;
+    }
+
+    //Métodos SET
+
+    public void setUser(String newUser){
+        this.user = newUser;
+    }
+    public void setPass(String newPassword){
+        this.password = newPassword;
+    }
+}
