84a44359f22484a779d43ef4fe86e6d66394759b
Pedro Cardoso <036pedro.cardoso@gmail.com>
2023-04-18 15:30:22 -0300

    Código refatorado, novo método adicionado e chamado no método main

--- Desafio.java
+++ Desafio.java
@@ -4,18 +4,19 @@ public class Desafio{
     static int num1, num2, num3;
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Insira o primeiro número: ");
-        num1 = scanner.nextInt();
-        System.out.println("Insira o segundo número: ");
-        num2 = scanner.nextInt();
-        System.out.println("Insira o terceiro número: ");
-        num3 = scanner.nextInt();
+        try(Scanner Scanner = new Scanner(System.in)){
+        num1 = Scanner.nextInt();
+        num2 = Scanner.nextInt();
+        num3 = Scanner.nextInt();
         
-        int maior1 = (num1 + num2 + Math.abs(num1 - num2))/2;
-        int maior2 = (maior1 + num3 + Math.abs(maior1 - num3))/2;
+    }
+}
+    static int maiorNum(int num1, int num2, int num3){
 
-        System.out.println(maior2 + " eh o maior");
+        int primeiroMaior = (num1 + num2 + Math.abs(num1 - num2))/2;
+        int maiorNumero = (primeiroMaior + num3 + Math.abs(primeiroMaior - num3))/2;
+
+        return maiorNumero;
 
     }
 }
